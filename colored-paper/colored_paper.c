02e3b79f6cb9ae28ab005d9156ed9b79ad23bd61
Taeung Song <treeze.taeung@gmail.com>
2016-01-13 16:42:45 +0900

    colored paper: organize frame for coding
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

---
+++ colored-paper/colored_paper.c
@@ -0,0 +1,39 @@
+/*
+ * Colored paper question
+ *
+ * Copyright (C) 2016, Taeung Song <treeze.taeung@gmail.com>
+ *
+ */
+#include <stdio.h>
+
+struct paper_group {
+	unsigned int value;
+	unsigned int paper[4];
+};
+
+struct group grouping_paper(struct paper_group g_papers[])
+{
+	struct group paper_group g_paper;
+
+	/* Group each paper with the rule that
+	 * same colored paper should be grouped.
+	 */
+
+	return g_paper;
+}
+
+struct group parse_paper_array(usigned int papers[])
+{
+	struct group paper_group g_paper;
+
+	/* Reading two line, assign each value to
+	 * each paper group following row and column.
+	 */
+
+	return g_paper;
+}
+
+int main(int argc, const char **argv)
+{
+	return 0;
+}
