5632fe085130d32b01ac888ec5a39c61573989c2
Taeung Song <treeze.taeung@gmail.com>
2016-01-15 22:01:33 +0900

    colored paper: Input and Output
    If inputing values, make papers list
    to count blue or white papers.
    And add output format.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- colored-paper/colored_paper.c
+++ colored-paper/colored_paper.c
@@ -6,10 +6,17 @@
  */
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_WIDTH 256
 
 struct paper_group {
 	unsigned int paper[4];
 };
+
+unsigned int white;
+unsigned int blue;
 
 void grouping_papers(unsigned int len, int **papers_list,
 		    int line_num, struct paper_group *row)
@@ -39,5 +46,45 @@ bool check_range(int len)
 
 int main(int argc, const char **argv)
 {
+	char input[MAX_WIDTH];
+	int len;
+
+	fgets(input, sizeof(input), stdin);
+	len = atoi(input);
+
+	if (check_range(len)) {
+		int **papers_list = (int **)malloc(sizeof(int *) * len);
+
+		for (int i = 0; i < len; i++) {
+			int end, index = 0;
+			int *row = (int *)malloc(sizeof(int) * len);
+
+			fgets(input, MAX_WIDTH, stdin);
+			end = strlen(input);
+
+			for (int j = 0; j < end; j++) {
+				char ch = input[j];
+				if (ch == '1' || ch == '0')
+					row[index++] = ch - '0';
+			}
+
+			if (index != len) {
+				printf("Error: wrong length of one side "
+				       "(N = %d).\n", len);
+				free(row);
+				for (int j; j < len; j++)
+					free(papers_list[j]);
+				free(papers_list);
+				return -1;
+			}
+			papers_list[i] = row;
+		}
+		parse_papers(len, papers_list);
+	} else
+		printf("\nError: wrong range of length.\n");
+
+	/* The numbers of white and blue paper */
+	printf("%d\n", white);
+	printf("%d\n", blue);
 	return 0;
 }
