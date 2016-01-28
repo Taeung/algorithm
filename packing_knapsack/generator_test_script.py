54479acc34f267a88f2d00e8220157f110c7cfaa
Taeung Song <treeze.taeung@gmail.com>
2016-01-28 15:08:21 +0900

    packing knapsack: bug fix, the number of times
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- packing_knapsack/generator_test_script.py
+++ packing_knapsack/generator_test_script.py
@@ -12,7 +12,7 @@ w = random.randrange(1,10000)
 test_script.write(str(nr)+" "+str(w)+"\\n\\\n")
 
 
-for i in range(1,nr-1):
+for i in range(1,nr):
     j_p = random.randrange(1,w)
     j_w = random.randrange(1,w)
     test_script.write(str(j_p)+" "+str(j_w)+"\\n\\\n")
