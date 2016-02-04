be9074485ff3a65c9c2f876ad80b5dafd4093825
Taeung Song <treeze.taeung@gmail.com>
2016-02-05 01:15:34 +0900

    quick sort: Implementations of quick_sort()
    Implement quick_sort() and merge_arr()
    But split_arr() is fall through.
    quick_sort() has functionallities that
    are split and merge but it can be performed
    when length is more than 3.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- quick_sort/quick_sort.c
+++ quick_sort/quick_sort.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define ASCEND true
 #define DESCEND false
@@ -16,12 +17,49 @@ bool sort_order;
 int *merge_arr(int *left, int med, int *right,
 	       int lft_len, int rgt_len)
 {
+	int *merged_arr = malloc(sizeof(int) * (lft_len + rgt_len));
+
+	memcpy(left, merged_arr, lft_len);
+	merged_arr[lft_len] = med;
+	memcpy(right, &merged_arr[lft_len+1], rgt_len);
+	free(left);
+	free(right);
+
+	return 0;
+}
+
+int split_arr(int *num_arr, int **left, int med,
+	      int **right, int len)
+{
 	return 0;
 }
 
 int *quick_sort(int *num_arr, int len)
 {
-	return 0;
+	int med, lft_len, rgt_len;
+	int *left, *right;
+	if (len == 1)
+		return num_arr;
+	else if (len == 2) {
+		if ((num_arr[0] < num_arr[1]) != sort_order) {
+			int temp = num_arr[0];
+
+			num_arr[0] = num_arr[1];
+			num_arr[1] = temp;
+		}
+		return num_arr;
+	}
+
+	med = num_arr[0];
+
+	lft_len = split_arr(num_arr, &left, med, &right, len);
+	rgt_len = len - lft_len;
+	free(num_arr);
+
+	left = quick_sort(left, lft_len);
+	right = quick_sort(right, rgt_len);
+
+	return merge_arr(left, med, right, lft_len, rgt_len);
 }
 
 int main(int argc, const char **argv)
