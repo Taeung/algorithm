7583b7915ad0c20e0bd69667ed3391a614f5b9d2
Taeung Song <treeze.taeung@gmail.com>
2016-02-10 22:41:13 +0900

    cheese: Add simple frame code for this problem
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

---
+++ cheese/cheese.c
@@ -0,0 +1,32 @@
+/*
+ * Cheese problem
+ *
+ * Copyright (C) 2016, Taeung Song <treeze.taeung@gmail.com>
+ *
+ */
+#include <stdio.h>
+#include <stdbool.h>
+
+struct cell{
+	bool has_cheese;
+	struct cell *next;
+};
+
+void melt_cheese_lines()
+{
+}
+
+struct cell *find_linked_cheeses(struct cell *cell)
+{
+	return cell;
+}
+
+bool contains(struct cell *cheese_lines[], struct cell cell)
+{
+	return true;
+}
+
+int main(int argc, const char **argv)
+{
+	return 0;
+}
