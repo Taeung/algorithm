7f4bb1ddeae4b1cf64597a02ec7abfa50ef15051
Taeung Song <treeze.taeung@gmail.com>
2016-03-16 00:57:10 +0900

    Write test evnironment information on test script
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- cheese/generator_test_script.py
+++ cheese/generator_test_script.py
@@ -1,10 +1,34 @@
 #!/bin/python
 import random
+import os
+import platform
 
-test_script = open("test.sh","w")
-test_script.write("#!/bin/bash\n")
+if platform.system() != 'Linux':
+    print "Can't generate test script on OS other than Linux"
+    exit()
+
+def get_gcc_version():
+    return os.popen('gcc --version').readline().rstrip()
+
+def get_gcc_options():
+    dirname = os.path.split(os.getcwd())[1]
+    filename = dirname + ".c"
+    return "gcc -ggdb " + filename + " -ansi -fno-asm -O2 -Wall -lm --static"
+
+def get_os_info():
+    return platform.platform()
+
+def test_script__new():
+    test_script = open("test.sh","w")
+    test_script.write("#!/bin/bash\n")
+    test_script.write("#\n# TEST ENVIRONMENT\n")
+    test_script.write("# gcc version: " + get_gcc_version() + "\n")
+    test_script.write("# gcc options: " + get_gcc_options() + "\n")
+    test_script.write("# os: " + get_os_info() + "\n\n")
+    return test_script
+
+test_script = test_script__new()
 test_script.write("echo -e \"")
-
 nr_col = random.randrange(3, 101)
 nr_row = random.randrange(3, 101)
 
