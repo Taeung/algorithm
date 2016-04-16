4f5bc996f9b246a012d7071479a960b5cbd017b0
Taeung Song <treeze.taeung@gmail.com>
2016-04-17 00:35:43 +0900

    cheese: New cheese.c changing a strategy to solve this problem
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

---
+++ cheese/cheese.c
@@ -0,0 +1,15 @@
+/*
+ * Cheese problem (New version)
+ *
+ * Copyright (C) 2016, Taeung Song <treeze.taeung@gmail.com>
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, const char **argv)
+{
+	return 0;
+}
