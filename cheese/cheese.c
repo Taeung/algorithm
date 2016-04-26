8c46a5c590a369c77635586edb4477e51599ef3b
Taeung Song <treeze.taeung@gmail.com>
2016-04-26 14:53:13 +0900

    cheese: Infrastructure code for this problem
    In order to solve this problem,
    initialize cheese_board object
    with __new(), __init() and __delete()
    In the near future, cheese_board object will
    be used with core functions for this algorithm.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- cheese/cheese.c
+++ cheese/cheese.c
@@ -29,6 +29,51 @@ struct range {
 	int col;
 };
 
+void cheese_board__delete(struct cheese_board *cheese_board)
+{
+	int i;
+
+	for (i = 0; i < cheese_board->row; i++)
+		free(cheese_board->board[i]);
+	free(cheese_board->board);
+	free(cheese_board);
+}
+
+void cheese_board__init(struct cheese_board *cheese_board, int row, int col)
+{
+	int i, j;
+	char input[MAX_WIDTH], *ptr, *arg;
+
+	cheese_board->row = row;
+	cheese_board->col = col;
+	for (i = 0; i < row; i++) {
+		struct cheese_cell *cheese_line = malloc(sizeof(struct cheese_cell) * col);
+
+		fgets(input, sizeof(input), stdin);
+		ptr = strdup(input);
+		if (!ptr) {
+			printf("%s: strdup failed\n", __func__);
+			cheese_board__delete(cheese_board);
+		}
+
+		for (j = 0; j < col; j++) {
+			arg = strsep(&ptr, " ");
+			cheese_line[j].has_cheese = atoi(arg);
+		}
+
+		cheese_board->board[i] = cheese_line;
+	}
+}
+
+struct cheese_board *cheese_board__new(int row, int col)
+{
+	struct cheese_board *cheese_board = malloc(sizeof(*cheese_board));
+
+	cheese_board->board = malloc(sizeof(struct cheese_cell *) * row);
+	cheese_board__init(cheese_board, row, col);
+	return cheese_board;
+}
+
 struct range get_range(void)
 {
 	char input[MAX_WIDTH], *ptr, *args;
@@ -62,14 +107,20 @@ bool check_range(int row, int col)
 
 int main(int argc, const char **argv)
 {
+	struct cheese_board *cheese_board;
 	struct range range = get_range();
 
 	if (check_range(range.row, range.col)) {
-		/* fall-through */
+		cheese_board = cheese_board__new(range.row, range.col);
+		if (!cheese_board) {
+			printf("Error: can't create cheese board\n");
+			return -1;
+		}
 	} else {
 		printf("Error: wrong range of input\n");
 		return -1;
 	}
 
+	cheese_board__delete(cheese_board);
 	return 0;
 }
