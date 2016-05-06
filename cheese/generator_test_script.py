6573489ae9edf3821848927700fc68f0ca4bdd60
Taeung Song <treeze.taeung@gmail.com>
2016-05-07 02:12:44 +0900

    cheese: Bug fix, order of first line of test input format
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- cheese/generator_test_script.py
+++ cheese/generator_test_script.py
@@ -32,25 +32,25 @@ test_script.write("echo -e \"")
 nr_col = random.randrange(3, 101)
 nr_row = random.randrange(3, 101)
 
-test_script.write(str(nr_col) + " " + str(nr_row) + "\\n\\\n")
+test_script.write(str(nr_row) + " " + str(nr_col) + "\\n\\\n")
 
-def write_zero_line(nr_row):
+def write_zero_line(nr_col):
     row = ""
-    for i in range(0, nr_row):
+    for i in range(0, nr_col):
         row += "0"
     test_script.write(row)
 
-write_zero_line(nr_row)
+write_zero_line(nr_col)
 test_script.write("\\n\\\n")
-nr_inner_col = nr_col - 2
 nr_inner_row = nr_row - 2
+nr_inner_col = nr_col - 2
 
-for i in range(0, nr_inner_col):
+for i in range(0, nr_inner_row):
     test_script.write("0")
-    for i in range(0, nr_inner_row):
+    for i in range(0, nr_inner_col):
         cheese_cell = random.randrange(0, 2)
         test_script.write(str(cheese_cell))
     test_script.write("0\\n\\\n")
 
-write_zero_line(nr_row)
+write_zero_line(nr_col)
 test_script.write("\\n\" | ./$1")
