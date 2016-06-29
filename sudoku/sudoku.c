e9125db894809c6f87726eed2feb45e2fdd7db89
Taeung Song <treeze.taeung@gmail.com>
2016-06-29 10:45:08 +0900

    sudoku: Introduce struct sudoku_cell
    Sudoku quiz has 9x9 board and each cell
    need a number, candidates, row info, col info
    and box info. So make struct sudoku_cell
    contain these data.
    
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -6,6 +6,17 @@
  */
 #include <stdio.h>
 
+#define MAX 9
+#define MAX_INPUT 24
+
+struct sudoku_cell {
+	int num;
+	int cand[MAX];
+	struct sudoku_cell *row;
+	struct sudoku_cell *col;
+	struct sudoku_cell *box;
+} sudoku[MAX][MAX];
+
 int main(int argc, const char **argv)
 {
 	return 0;
