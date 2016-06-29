709a372ca5c31f93cf2784674c8365e5a63299fb
Taeung Song <treeze.taeung@gmail.com>
2016-06-29 11:36:34 +0900

    sudoku: Initialize sudoku quiz
    Signed-off-by: Taeung Song <treeze.taeung@gmail.com>

--- sudoku/sudoku.c
+++ sudoku/sudoku.c
@@ -5,6 +5,7 @@
  *
  */
 #include <stdio.h>
+#include <ctype.h>
 
 #define MAX 9
 #define MAX_INPUT 24
@@ -17,7 +18,29 @@ struct sudoku_cell {
 	struct sudoku_cell *box;
 } sudoku[MAX][MAX];
 
+int sudoku__init()
+{
+	int i, row, col;
+	char input[MAX_INPUT] = {0};
+
+	for (row = 0; row < MAX; row++) {
+		fgets(input, sizeof(input), stdin);
+		col = 0;
+		for (i = 0; i < MAX_INPUT; i++) {
+			int ch = input[i];
+
+			if (isdigit(ch))
+				sudoku[row][col++].num = input[i] - '0';
+		}
+
+	}
+
+	return 0;
+}
+
 int main(int argc, const char **argv)
 {
+	sudoku__init();
+
 	return 0;
 }
